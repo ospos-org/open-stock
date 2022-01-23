e6137b85703cb857715fe8e723f3112aa4b15b1c
UnRealReincarlution <benwhite2413@gmail.com>
2022-01-24 10:59:59 +1300

    Redis Query and Responce through Serialized JSON

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,7 @@
 use chrono::prelude::*;
+use std::fmt;
+use redis::{self};
+use serde::{Serialize, Deserialize};
 
 pub fn title(input: &str) -> String {
     return format!("{}{}{}", "[", input.to_uppercase(), "]");
@@ -9,3 +12,33 @@ pub fn log(origin: &str, descriptor: &str) {
 
     println!("{} {} - {}",time.format("%Y-%m-%d %H:%M:%S").to_string(), title(origin), descriptor);
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Product {
+    pub base_name: String,
+    pub id: i64,
+    pub handle: String,
+    pub variations: Vec<Variation>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Variation {
+    pub stock_locations: Vec<Location>,
+    pub price: f32,
+    pub name: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Location {
+    pub l_id: u64,
+    pub items: i64
+}
+
+// impl fmt::Debug for Product {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         f.debug_struct("Location")
+//          .field("Name", &self.base_name)
+//          .field("ID", &self.id)
+//          .finish()
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,9 @@
-use redis::{self, transaction, Commands};
-
-use std::collections::HashMap;
+use redis::{self};
 use std::env;
+use serde::{Serialize, Deserialize};
+use serde_json;
 
 mod helpers; 
-
-/// This function demonstrates how a return value can be coerced into a
-/// hashmap of tuples.  This is particularly useful for responses like
-/// CONFIG GET or all most H functions which will return responses in
-/// such list of implied tuples.
-fn do_print_max_entry_limits(con: &mut redis::Connection) -> redis::RedisResult<()> {
-    // since rust cannot know what format we actually want we need to be
-    // explicit here and define the type of our response.  In this case
-    // String -> int fits all the items we query for.
-    let config: HashMap<String, isize> = redis::cmd("CONFIG")
-        .arg("GET")
-        .arg("*-max-*-entries")
-        .query(con)?;
-
-    println!("Max entry limits:");
-
-    println!(
-        "  max-intset:        {}",
-        config.get("set-max-intset-entries").unwrap_or(&0)
-    );
-    println!(
-        "  hash-max-ziplist:  {}",
-        config.get("hash-max-ziplist-entries").unwrap_or(&0)
-    );
-    println!(
-        "  list-max-ziplist:  {}",
-        config.get("list-max-ziplist-entries").unwrap_or(&0)
-    );
-    println!(
-        "  zset-max-ziplist:  {}",
-        config.get("zset-max-ziplist-entries").unwrap_or(&0)
-    );
-
-    Ok(())
-}
-
-/// This is a pretty stupid example that demonstrates how to create a large
-/// set through a pipeline and how to iterate over it through implied
-/// cursors.
-fn do_show_scanning(con: &mut redis::Connection) -> redis::RedisResult<()> {
-    // This makes a large pipeline of commands.  Because the pipeline is
-    // modified in place we can just ignore the return value upon the end
-    // of each iteration.
-    let mut pipe = redis::pipe();
-    for num in 0..1000 {
-        pipe.cmd("SADD").arg("my_set").arg(num).ignore();
-    }
-
-    // since we don't care about the return value of the pipeline we can
-    // just cast it into the unit type.
-    pipe.query(con)?;
-
-    // since rust currently does not track temporaries for us, we need to
-    // store it in a local variable.
-    let mut cmd = redis::cmd("SSCAN");
-    cmd.arg("my_set").cursor_arg(0);
-
-    // as a simple exercise we just sum up the iterator.  Since the fold
-    // method carries an initial value we do not need to define the
-    // type of the iterator, rust will figure "int" out for us.
-    let sum: i32 = cmd.iter::<i32>(con)?.sum();
-
-    println!("The sum of all numbers in the set 0-1000: {}", sum);
-
-    Ok(())
-}
-
-/// Demonstrates how to do an atomic increment in a very low level way.
-fn do_atomic_increment_lowlevel(con: &mut redis::Connection) -> redis::RedisResult<()> {
-    let key = "the_key";
-    println!("Run low-level atomic increment:");
-
-    // set the initial value so we have something to test with.
-    redis::cmd("SET").arg(key).arg(42).query(con)?;
-
-    loop {
-        // we need to start watching the key we care about, so that our
-        // exec fails if the key changes.
-        redis::cmd("WATCH").arg(key).query(con)?;
-
-        // load the old value, so we know what to increment.
-        let val: isize = redis::cmd("GET").arg(key).query(con)?;
-
-        // at this point we can go into an atomic pipe (a multi block)
-        // and set up the keys.
-        let response: Option<(isize,)> = redis::pipe()
-            .atomic()
-            .cmd("SET")
-            .arg(key)
-            .arg(val + 1)
-            .ignore()
-            .cmd("GET")
-            .arg(key)
-            .query(con)?;
-
-        match response {
-            None => {
-                continue;
-            }
-            Some(response) => {
-                let (new_val,) = response;
-                println!("  New value: {}", new_val);
-                break;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-/// Demonstrates how to do an atomic increment with transaction support.
-fn do_atomic_increment(con: &mut redis::Connection) -> redis::RedisResult<()> {
-    let key = "the_key";
-    println!("Run high-level atomic increment:");
-
-    // set the initial value so we have something to test with.
-    con.set(key, 42)?;
-
-    // run the transaction block.
-    let (new_val,): (isize,) = transaction(con, &[key], |con, pipe| {
-        // load the old value, so we know what to increment.
-        let val: isize = con.get(key)?;
-        // increment
-        pipe.set(key, val + 1).ignore().get(key).query(con)
-    })?;
-
-    // and print the result
-    println!("New value: {}", new_val);
-
-    Ok(())
-}
 
 /// Runs all the examples and propagates errors up.
 fn start_api(url: &str) -> redis::RedisResult<()> {
@@ -142,15 +11,45 @@ fn start_api(url: &str) -> redis::RedisResult<()> {
     let client = redis::Client::open(url)?;
     let mut con = client.get_connection()?;
 
-    // read some config and print it.
-    do_print_max_entry_limits(&mut con)?;
+    helpers::log("redis", "connected");
+    
+    let demo = helpers::Product {
+        base_name: "demo_product".to_string(),
+        id: 019238190283018,
+        handle: "pd25932918".to_string(),
+        variations: vec![
+            helpers::Variation {
+                stock_locations: vec![
+                    helpers::Location {
+                        l_id: 1237197395129,
+                        items: 15
+                    }
+                ],
+                price: 162.99,
+                name: "demo_product small".to_string()
+            }
+        ]
+    };
 
-    // demonstrate how scanning works.
-    do_show_scanning(&mut con)?;
+    let response: Option<(String,)> = redis::pipe()
+        .atomic()
+        .cmd("SET").arg(demo.id).arg(serde_json::to_string(&demo).unwrap()).ignore()
+        .cmd("GET").arg(demo.id)
+        .query(&mut con)?;
 
-    // shows an atomic increment.
-    do_atomic_increment_lowlevel(&mut con)?;
-    do_atomic_increment(&mut con)?;
+    match response {
+        None => {
+            helpers::log("redis", "no response");
+        }
+        Some(response) => {
+            let (serialized, ) = response;
+
+            let json: helpers::Product = serde_json::from_str(&serialized.to_string()).unwrap();
+            println!("{:?}", json);
+
+            // helpers::log("redis", json)
+        }
+    }
 
     Ok(())
 }
@@ -163,8 +62,6 @@ fn main() {
     } else {
         "redis://127.0.0.1:6379/"
     };
-
-    helpers::log("redis", "connected");
 
     match start_api(url) {
         Err(err) => {
