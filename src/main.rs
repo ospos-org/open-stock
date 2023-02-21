195707e628f4b5b3ca74a361ba1dbee6c8ca5f92
Ben White <contact@bennjii.dev>
2023-02-22 09:58:44 +1100

    mod: env variable for allowed origin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use pool::Db;
 use rocket::{*, fairing::{Fairing, Info, Kind}, http::Header};
 use sea_orm_rocket::Database;
-use rocket::config::Config;
 
 pub(crate) mod methods;
 pub(crate) mod entities;
@@ -22,7 +21,9 @@ impl Fairing for CORS {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "https://open-retail.bennjii.dev"));
+        let access_origin = dotenv::var("ACCESS_ORIGIN").unwrap();
+
+        response.set_header(Header::new("Access-Control-Allow-Origin", access_origin));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
@@ -32,10 +33,6 @@ impl Fairing for CORS {
 #[launch] // The "main" function of the program
 fn rocket() -> _ {
     dotenv::dotenv().ok();
-
-//    let figment = Config::figment()
-//        .merge(("tls.certs", "keys/certs.pem"))
-//        .merge(("tls.key", "keys/key.pem"));
 
     rocket::build()
         .attach(Db::init())
@@ -47,5 +44,4 @@ fn rocket() -> _ {
         .mount("/api/supplier", methods::supplier::handlers::routes())
         .mount("/api/store", methods::store::handlers::routes())
         .mount("/api/helpers", methods::helpers::handlers::routes())
-//        .configure(figment)
 }
