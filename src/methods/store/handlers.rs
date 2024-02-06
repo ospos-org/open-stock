d335ac9afe970d8ea4bfbbc94ecdfd6b205e2931
Ben White <contact@bennjii.dev>
2024-02-06 17:17:03 +1100

    mod: stores

--- src/methods/store/handlers.rs
+++ src/methods/store/handlers.rs
@@ -4,12 +4,14 @@ use rocket::{get, http::CookieJar, post, serde::json::Json};
 use rocket_db_pools::Connection;
 use rocket_okapi::settings::OpenApiSettings;
 use rocket_okapi::{openapi, openapi_get_routes_spec};
+use open_stock::{InternalDb, Session};
 
 use crate::{
     check_permissions,
     methods::{cookie_status_wrapper, Action, Error, ErrorResponse},
     pool::Db,
 };
+use crate::guards::Convert;
 
 use super::Store;
 
@@ -22,102 +24,53 @@ pub fn documented_routes(settings: &OpenApiSettings) -> (Vec<rocket::Route>, Ope
 #[openapi(tag = "Store")]
 #[get("/")]
 pub async fn get_all(
-    conn: Connection<Db>,
-    cookies: &CookieJar<'_>,
-) -> Result<Json<Vec<Store>>, Error> {
-    let db = conn.into_inner();
-
-    let session = cookie_status_wrapper(&db, cookies).await?;
+    db: InternalDb,
+    session: Session,
+) -> Convert<Vec<Store>> {
     check_permissions!(session.clone(), Action::FetchStore);
-
-    match Store::fetch_all(session, &db).await {
-        Ok(stores) => Ok(Json(stores)),
-        Err(reason) => Err(ErrorResponse::db_err(reason)),
-    }
+    Store::fetch_all(session, &db.0).await.into()
 }
 
 #[openapi(tag = "Store")]
 #[get("/<id>")]
 pub async fn get(
-    conn: Connection<Db>,
+    db: InternalDb,
+    session: Session,
     id: &str,
-    cookies: &CookieJar<'_>,
-) -> Result<Json<Store>, Error> {
-    let db = conn.into_inner();
-
-    let session = cookie_status_wrapper(&db, cookies).await?;
+) -> Convert<Store> {
     check_permissions!(session.clone(), Action::FetchStore);
-
-    match Store::fetch_by_id(id, session, &db).await {
-        Ok(store) => Ok(Json(store)),
-        Err(reason) => Err(ErrorResponse::db_err(reason)),
-    }
+    Store::fetch_by_id(id, session, &db.0).await.into()
 }
 
 #[openapi(tag = "Store")]
 #[get("/code/<code>")]
 pub async fn get_by_code(
-    conn: Connection<Db>,
+    db: InternalDb,
+    session: Session,
     code: &str,
-    cookies: &CookieJar<'_>,
-) -> Result<Json<Store>, Error> {
-    let db = conn.into_inner();
-
-    let session = cookie_status_wrapper(&db, cookies).await?;
+) -> Convert<Store> {
     check_permissions!(session.clone(), Action::FetchStore);
-
-    match Store::fetch_by_code(code, session, &db).await {
-        Ok(store) => Ok(Json(store)),
-        Err(reason) => Err(ErrorResponse::db_err(reason)),
-    }
+    Store::fetch_by_code(code, session, &db.0).await.into()
 }
 
 #[openapi(tag = "Store")]
 #[post("/generate")]
 async fn generate(
-    conn: Connection<Db>,
-    cookies: &CookieJar<'_>,
-) -> Result<Json<Vec<Store>>, Error> {
-    let db = conn.into_inner();
-
-    let session = cookie_status_wrapper(&db, cookies).await?;
+    db: InternalDb,
+    session: Session,
+) -> Convert<Vec<Store>> {
     check_permissions!(session.clone(), Action::GenerateTemplateContent);
-
-    match Store::generate(session, &db).await {
-        Ok(res) => Ok(Json(res)),
-        Err(err) => Err(ErrorResponse::db_err(err)),
-    }
+    Store::generate(session, &db.0).await.into()
 }
 
 #[openapi(tag = "Store")]
 #[post("/<id>", data = "<input_data>")]
 async fn update(
-    conn: Connection<Db>,
-    id: &str,
-    cookies: &CookieJar<'_>,
+    db: InternalDb,
+    session: Session,
     input_data: Validated<Json<Store>>,
+    id: &str,
 ) -> Result<Json<Store>, Error> {
-    let input_data = input_data.clone().0.into_inner();
-    let db = conn.into_inner();
-
-    let session = cookie_status_wrapper(&db, cookies).await?;
     check_permissions!(session.clone(), Action::ModifyStore);
-
-    if session
-        .clone()
-        .employee
-        .level
-        .into_iter()
-        .find(|x| x.action == Action::ModifyStore)
-        .unwrap()
-        .authority
-        >= 1
-    {
-        match Store::update(input_data, session, id, &db).await {
-            Ok(res) => Ok(Json(res)),
-            Err(reason) => Err(ErrorResponse::db_err(reason)),
-        }
-    } else {
-        Err(ErrorResponse::unauthorized(Action::ModifyStore))
-    }
+    Store::update(input_data.data(), session, id, &db.0).await.into()
 }
